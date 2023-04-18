0bbc1164422eada3387ef96c4b7b2e4a3cf01492
Tom <thomasjhouf@gmail.com>
2023-04-18 18:03:21 -0400

    starting day 2 with a navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
+const styles = {
+	btnFloating: {
+		borderRadius: '40px',
+		width: '203px',
+		height: '203px',
+		display: 'flex',
+	},
+
+	btnFloatingImg: {
+		width: '100%',
+		height: '100%',
+	}
+}
 
 
 function Nav() {
@@ -11,48 +24,56 @@ function Nav() {
 
 		if (Auth.loggedIn()) {
 			return (
-				<ul className="flex-row">
-					<li className="mx-1">
-						<Link to="/Mexico">Mexico</Link>
-					</li>
-					<li className="mx-1">
-						<Link to="/Germany">Germany</Link>
-					</li>
-					<li className="mx-1">
-						<Link to="/Japan">Japan</Link>
-					</li>
-					<li className="mx-1">
-						<Link to="/Australia">Australia</Link>
-					</li>
-					<li className="mx-1">
-						<a href="/" onClick={() => Auth.logout()}>
-							Logout
-						</a>
-					</li>
-				</ul>
+				<nav>
+					<div className="nav-wrapper">
+						<ul id="nav-mobile" class="right hide-on-med-and-down">
+							<li>
+								<Link to="/Mexico">Mexico</Link>
+							</li>
+							<li>
+								<Link to="/Germany">Germany</Link>
+							</li>
+							<li>
+								<Link to="/Japan">Japan</Link>
+							</li>
+							<li>
+								<Link to="/Australia">Australia</Link>
+							</li>
+							<li>
+								<a href="/" onClick={() => Auth.logout()}>
+									Logout
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
 			);
 		} else {
 			return (
-				<ul className="flex-row">
-					<li className="mx-1">
-						<Link to="/signup">Signup</Link>
-					</li>
-					<li className="mx-1">
-						<Link to="/login">Login</Link>
-					</li>
-					<li className="mx-1">
-						<Link to="/Mexico">Mexico</Link>
-					</li>
-					<li className="mx-1">
-						<Link to="/Germany">Germany</Link>
-					</li>
-					<li className="mx-1">
-						<Link to="/Japan">Japan</Link>
-					</li>
-					<li className="mx-1">
-						<Link to="/Australia">Australia</Link>
-					</li>
-				</ul>
+				<nav>
+					<div className="nav-wrapper">
+						<ul id="nav-mobile" class="right hide-on-med-and-down">
+							<li>
+								<Link to="/signup">Signup</Link>
+							</li>
+							<li>
+								<Link to="/login">Login</Link>
+							</li>
+							<li>
+								<Link to="/Mexico">Mexico</Link>
+							</li>
+							<li>
+								<Link to="/Germany">Germany</Link>
+							</li>
+							<li>
+								<Link to="/Japan">Japan</Link>
+							</li>
+							<li>
+								<Link to="/Australia">Australia</Link>
+							</li>
+						</ul>
+					</div>
+				</nav>
 			);
 		}
 	}
