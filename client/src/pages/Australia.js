f17c707f3b5b9e73059790d5c867d26de1619183
ScrimmyFS <dscrimshaw12345@gmail.com>
2023-04-24 20:32:10 -0400

    Got the cards working and added some css

--- client/src/pages/Australia.js
+++ client/src/pages/Australia.js
@@ -1,37 +1,35 @@
 import React from 'react';
 import { useQuery, } from '@apollo/client';
 import { QUERY_PRODUCT_COUNTRY } from '../utils/queries'
+import "../components/cssFiles/Cards.css"
 
 const Australia = () => {
 	const { loading, data } = useQuery(QUERY_PRODUCT_COUNTRY, {
-		variables: { country: "mexico" }
+		variables: { country: "australia" }
 	})
    console.log(loading)
-	const product = data?.products || [];
+	const products = data?.country || [];
 
 
 	return (
 
 
 		<div className="container">
-			<div class="row">
-				<div class="col s12 m6">
-					<div class="card">
-						{product.map((product) => (
-							<div>
-								<div class="card-image">
-									<img alt="product" src={`${product.image}`}></img>
-									<span class="card-title">{`${product.item_name}`}</span>
-									{/* <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a> */}
-								</div>
-								<div class="card-content">
-									<p>{`${product.description}`}</p>
-								</div>
+			<div className="row">
+				{products.map((product) => (
+					<div className="col s12 m3">
+						<div className="card">
+							<div className="card-image">
+								<img alt="product" src={`${product.image}`}></img>
+								<span className="">{`${product.itemName}`}</span>
+								<button onClick="" className="btn-floating halfway-fab waves-effect waves-light red" href="#"><i class="material-icons">add</i></button>
 							</div>
-						))}
-
+							<div className="card-content">
+								<p>{`${product.description}`}</p>
+							</div>
+						</div>
 					</div>
-				</div>
+				))}
 			</div>
 		</div>
 	);
