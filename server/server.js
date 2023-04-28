3fd94a45877ffa85c0f1eb8b844a2d83bfd27bd8
Tom <thomasjhouf@gmail.com>
2023-04-27 21:03:58 -0400

    last time U swear

--- server/server.js
+++ server/server.js
@@ -19,15 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-//app.use('/images', express.static(path.join(__dirname, '../client/images')));
-//
-//if (process.env.NODE_ENV === 'production') {
-//	app.use(express.static(path.join(__dirname, '../client/build')));
-//}
-//
-//app.get('*', (req, res) => {
-//	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//});
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
